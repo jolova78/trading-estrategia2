6b6f549ac9545b7cb62157e4ef2affa5e4c59f99
jolova78 <jolopez07@outlook.com>
2026-08-02 18:45:09 -0600

    Execute pending orders before generating new ones
    Rearranged order of executing and generating orders.

--- motor_estrategia.py
+++ motor_estrategia.py
@@ -737,10 +737,17 @@ def ejecutar():
         fecha_senal
     )
 
-    ordenes = generar_ordenes(
-        estado,
-        senales
-    )
+    # Ejecutar órdenes pendientes de la sesión anterior
+estado = ejecutar_ordenes(
+    estado,
+    datos
+)
+
+# Generar las órdenes para la próxima apertura
+ordenes = generar_ordenes(
+    estado,
+    senales
+)
 
     capital = capital_estimado(
         estado,
